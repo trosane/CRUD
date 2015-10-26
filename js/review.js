fa0d38d27a0832ffb3caf7052c09145d7754e713
Trent Rosane <trosane@uw.edu>
2015-10-26 09:38:29 -0700

    Parse functionality

---
+++ js/review.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+    Parse.initialize("xRcpayJ1Hgmw3n9hQJTdA43Igz9r2KlQC9I4QcfK", "rLl6JFzFA7C6Qbc9DUIyeTGbh8LDpeAGTh6h6Sz1");
+
+var Review = Parse.Object.extend("Review");
+
+$('form').submit(function() {
+	var review = new Review();
+
+	review.set('title', $("#title-area").val());
+	review.set('comment', $("#comment-area").val());
+
+	review.save(null, function() {
+		success:getData()
+	});
+
+	return false;
+})
+
+var getData = function() {
+	var query = new Parse.Query(Review);
+
+	query.find({
+		success:function(response) {
+			buildList(response);
+		}
+	});
+}
+
+var buildList = function(data) {
+	$('#reviews').empty();
+	data.forEach(function(d) {
+		addItem(d);
+	});
+}
+
+var addItem = function(item) {
+	var title = item.get("title");
+	var comment = item.get("comment");
+	var titleText = '<h2 class="title">' + title + '</h2>';
+	var date = '<p class="date">time</p>';
+	var commentText = '<p class="comment">' + comment + '</p>';
+	var vote = '<p class="date">votes</p>';
+	var reviewDiv = $('<div class="panel panel-default" id="review-block">' + titleText + date + commentText + vote + '</div>');
+	$('#reviews').append(reviewDiv);
+}
+
+getData();
+})
